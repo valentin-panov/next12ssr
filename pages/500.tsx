5bc5c52bf7020f3eb393d4b24dd94a2ec3a88fb7
Valentin Panov <valentin.panov@me.com>
2024-04-18 15:52:16 +0200

    working stand

---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const E500: React.FC = () => {
+  return <div>ERROR 500</div>;
+};
+
+export default E500;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,42 @@
 import React from "react";
 
-type TData = {
-  data: { userId: string; id: string; title: string; completed: string };
-};
+// type TData = {
+//   data: { userId: string; id: string; title: string; completed: string };
+// };
+
+type TProps = { message: string };
 
 // This gets called on every request
 export async function getServerSideProps() {
-  let data: string = "";
   // Fetch data from external API
   try {
-    data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
-      (response) => response.json(),
-    );
-  } catch (e) {
-    console.error(e);
+    const { title } = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    ).then((response) => response.json());
+    // Pass data to the page via props
+    return { props: { message: title } };
+  } catch ({ message }) {
+    console.error(message);
+    // Pass data to the page via props
+    return {
+      redirect: {
+        destination: "/500",
+        permanent: false,
+      },
+    };
   }
-
-  // Pass data to the page via props
-  return { props: { data } };
 }
 
-const Index: React.FC<TData> = ({ data }) => {
-  return <div>Incoming data: {data.title}</div>;
+const Index: React.FC<TProps> = ({ message }) => {
+  console.log("FIND ME!");
+  // useEffect(() => {
+  //   if (message) {
+  //     window.location.href = "/500";
+  //     console.error("!!!");
+  //   }
+  // }, [message]);
+
+  return <div>Incoming data: {message}</div>;
 };
+
 export default Index;
